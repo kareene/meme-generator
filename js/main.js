c193602bfee40c67590f47ffbf7b13ac8c544645
Kareene Komet <kareene@kareene.com>
2020-02-12 19:49:02 +0200

    end of day 1

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ var gCtx;
 function onInit() {
     gCanvas = document.querySelector('#meme-canvas');
     gCtx = gCanvas.getContext('2d');
-    resizeCanvas();
-    onCreateMeme(3)
+    renderImgs();
+    // onCreateMeme(3)
 }
 
 function resizeCanvas() {
@@ -16,34 +16,65 @@ function resizeCanvas() {
     gCanvas.height = elContainer.offsetWidth;
 }
 
+function renderImgs() {
+    var imgs = getImgsForDisplay();
+    var strHTMLs = imgs.map(img => {
+        return `<img src="${img.url}" class="img-${img.id}" onclick="onCreateMeme(${img.id})">`
+    });
+    document.querySelector('.images-container').innerHTML = strHTMLs.join('');
+}
+
 function onCreateMeme(imgId) {
+    document.querySelector('.gallery-container').style.display = 'none';
+    document.querySelector('.editor-container').style.display = 'flex';
     createMeme(imgId);
     renderMeme();
 }
 
 function renderMeme() {
+    resizeCanvas();
     var meme = getMeme();
-    var img = new Image();
-    img.src = getImg(meme.selectedImgId).url;
-    img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    console.log(meme);
+    // var img = new Image();
+    // img.src = getImg(meme.selectedImgId).url;
+    var elImg = document.querySelector(`.img-${meme.selectedImgId}`);
+    console.log(elImg);
+    console.log(gCtx);
+    // elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height);
         meme.lines.forEach((line, idx) => {
             let text = line.txt;
             gCtx.fillStyle = line.color;
             gCtx.font = `${line.size}px Impact`;
             gCtx.textAlign = line.align;
-            let posX = (line.align === 'left') ? 20 : gCanvas.width - 20;
+            let posX = (line.align === 'left') ? 20 : (line.align === 'right') ? gCanvas.width - 20 : gCanvas.width / 2;
             let posY = (idx === 0) ? line.size : gCanvas.height - line.size / 2;
             gCtx.lineWidth = '2';
             gCtx.strokeStyle = 'black';
             gCtx.strokeText(text, posX, posY);
             gCtx.fillText(text, posX, posY);
         });
-    }
+    // }
 }
 
 function onUpdateMemeText() {
     var elTextInput = document.querySelector('[name="meme-text"]');
     updateMemeText(elTextInput.value);
+    renderMeme();
+}
+
+function onUpdateTextAlign(align) {
+    updateTextAlign(align);
+    renderMeme();
+}
+
+function onUpdateTextSize(diff) {
+    updateTextSize(diff);
+    renderMeme();
+}
+
+function onUpdateFillColor(value) {
+    var elFillColor = document.querySelector('[name="fill-color"]');
+    updateFillColor(value);
     renderMeme();
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,8 +1,29 @@
 'use strict';
 
+const NUM_OF_IMGS = 18;
 var gKeywords = {'happy': 12,'funny puk': 1}
-var gImgs = [{id: 3, url: 'img/3.jpg', keywords: ['happy']}];
+var gImgs = createImgs();
 var gMeme;
+
+function createImgs() {
+    var imgs = [];
+    for (let i = 1; i <= NUM_OF_IMGS; i++) {
+        imgs.push(createImg(i));
+    }
+    return imgs;
+}
+
+function createImg(id) {
+    return {
+        id: id,
+        url: `img/${id}.jpg`,
+        keywords: []
+    };
+}
+
+function getImgsForDisplay() {
+    return gImgs;
+}
 
 function getImg (imgId) {
     return gImgs.find(img => img.id === imgId)
@@ -15,13 +36,13 @@ function createMeme(imgId) {
         lines: [{
             txt: 'I never eat Falafel',
             size: 40,
-            align: 'left',
+            align: 'center',
             color: 'red'
         },
         {
             txt: 'I always eat chocholate',
             size: 40,
-            align: 'left',
+            align: 'center',
             color: 'red'
         }]
     };
@@ -33,4 +54,19 @@ function getMeme() {
 
 function updateMemeText(txt) {
     gMeme.lines[gMeme.selectedLineIdx].txt = txt;
+}
+
+function updateTextAlign(align) {
+    gMeme.lines[gMeme.selectedLineIdx].align = align;
+}
+
+function updateTextSize(diff) {
+    var currLine = gMeme.lines[gMeme.selectedLineIdx];
+    if (currLine.size + diff > 0) {
+        currLine.size += diff;
+    }
+}
+
+function updateFillColor(value) {
+    gMeme.lines[gMeme.selectedLineIdx].color = value;
 }
