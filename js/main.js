3562328dd1299b3abe3d47e6bdf1e60d35553292
Kareene Komet <kareene@kareene.com>
2020-02-12 15:23:27 +0200

    nothing is ready

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
 'use strict';
+
+var gCanvas;
+var gCtx;
+
+function onInit() {
+    gCanvas = document.querySelector('#meme-canvas');
+    gCtx = gCanvas.getContext('2d');
+    resizeCanvas();
+    onCreateMeme(3)
+}
+
+function resizeCanvas() {
+    var elContainer = document.querySelector('.editor-img');
+    gCanvas.width = elContainer.offsetWidth;
+    gCanvas.height = elContainer.offsetWidth;
+}
+
+function onCreateMeme(imgId) {
+    createMeme(imgId);
+    renderMeme();
+}
+
+function renderMeme() {
+    var meme = getMeme();
+    var img = new Image();
+    img.src = getImg(meme.selectedImgId).url;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+        meme.lines.forEach((line, idx) => {
+            let text = line.txt;
+            gCtx.fillStyle = line.color;
+            gCtx.font = `${line.size}px Impact`;
+            gCtx.textAlign = line.align;
+            let posX = (line.align === 'left') ? 20 : gCanvas.width - 20;
+            let posY = (idx === 0) ? line.size : gCanvas.height - line.size / 2;
+            gCtx.lineWidth = '2';
+            gCtx.strokeStyle = 'black';
+            gCtx.strokeText(text, posX, posY);
+            gCtx.fillText(text, posX, posY);
+        });
+    }
+}
+
+function onUpdateMemeText() {
+    var elTextInput = document.querySelector('[name="meme-text"]');
+    updateMemeText(elTextInput.value);
+    renderMeme();
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,36 @@
 'use strict';
+
+var gKeywords = {'happy': 12,'funny puk': 1}
+var gImgs = [{id: 3, url: 'img/3.jpg', keywords: ['happy']}];
+var gMeme;
+
+function getImg (imgId) {
+    return gImgs.find(img => img.id === imgId)
+}
+
+function createMeme(imgId) {
+    gMeme = {
+        selectedImgId: imgId,
+        selectedLineIdx: 0,
+        lines: [{
+            txt: 'I never eat Falafel',
+            size: 40,
+            align: 'left',
+            color: 'red'
+        },
+        {
+            txt: 'I always eat chocholate',
+            size: 40,
+            align: 'left',
+            color: 'red'
+        }]
+    };
+}
+
+function getMeme() {
+    return gMeme;
+}
+
+function updateMemeText(txt) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = txt;
+}
